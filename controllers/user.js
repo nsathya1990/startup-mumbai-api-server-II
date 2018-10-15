9f3910f1650a55216c56423623ba5ae477bcbab3
Aniket Hemant Shinde <aniketshinde.work@gmail.com>
2018-10-15 13:09:47 +0530

    solving the issues in login

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,9 @@
 const user = require('../models/user');
 const jwt = require('jwt-simple');
 
-;
 
 exports.postLoginUser = (req, res ,next) =>{
     user.findOne({ email: req.body.email }, function(err, user) {
-<<<<<<< HEAD
       if (!user) return res.json({
         status:401,
         message: 'User does not exist'
@@ -23,11 +21,6 @@ exports.postLoginUser = (req, res ,next) =>{
         res.json({
             message: "password"
         });
-=======
-      if (!user) return res.json(401, 'User does not exist');
-      user.comparePassword(req.body.password, function(err, isMatch) {
-        if (!isMatch) return res.json(401, 'Invalid email and/or password');
->>>>>>> 0cdbf06cf5ad1df6e837c57d64e9b0ed24d3e180
         var token = createJwtToken(user);
         res.json({ token: token });
       });
