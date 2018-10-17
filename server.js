33c70ba55d3fa4df746cd1fca23700546d0f3cd8
Aniket Hemant Shinde <aniketshinde.work@gmail.com>
2018-10-17 19:22:34 +0530

    modified the mongoose connection

--- server.js
+++ server.js
@@ -40,13 +40,13 @@ const app = express();
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useNewUrlParser', true);
-// mongoose.connect(process.env.MONGO_URL, {
-//   auth: {
-//     user: process.env.MONGO_DB_USER,
-//     password: process.env.MONGO_DB_PASSWORD
-//   }
-// })
-mongoose.connect('mongodb://localhost:27017/tvdb');
+mongoose.connect(process.env.MONGO_URL, {
+  auth: {
+    user: process.env.MONGO_DB_USER,
+    password: process.env.MONGO_DB_PASSWORD
+  }
+})
+// mongoose.connect('mongodb://localhost:27017/tvdb');
 mongoose.connection.on('error', (err) => {
   console.error(err);
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
